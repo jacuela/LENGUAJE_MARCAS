d2383aee5bf50bc0994554ebd8299e7e7c0e6571
jacuela <jacuela@gmail.com>
2024-01-18 14:05:00 +0100

    jueves

---
+++ XML/01_pruebas_iniciales/ver.php
@@ -0,0 +1,32 @@
+<?php
+
+$xml = simplexml_load_file('listaclientes.xml');
+
+//Listado de clientes
+print("<h3>Listado de clientes</h3>");
+$clientes = $xml->xpath("//cliente");
+foreach ($clientes as $cliente) {
+    print("Nombre:" . $cliente->nombre . "<br>");
+    print("CIF:" . $cliente->cif . "<br>");
+    if ($cliente->diasentrega != "") {
+        print("diasentrega:" . $cliente->diasentrega . "<br>");
+    } else {
+        print("diasentrega: SIN DIAS<br>");
+    }
+
+    echo "<hr>";
+}
+
+//Listado de clientes
+print("<h3>Listado de nombres de cliente</h3>");
+$resul = $xml->xpath("//nombre");
+
+// print("<pre>");
+// print_r($resul);
+// print("</pre>");
+
+print "<ol>";
+foreach ($resul as $nombre) {
+    print("<li>" . $nombre . "</li>");
+}
+print "</ol>";
